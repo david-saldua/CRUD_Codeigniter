0002612c8eaff27a5844b3d73f855c9346242ae7
David Groundlink <davidsaldua24@gmail.com>
2023-02-27 14:40:38 +0800

    add views

---
+++ app/Views/home.php
@@ -0,0 +1,5 @@
+<div class="card card-outline card-primary rounded-0 mt-5">
+    <div class="card-booty">
+        <h1 class="text-center">Welcome to CI4 Simple CRUD Application </h1>
+    </div>
+</div>
---
+++ app/Views/list.php
@@ -0,0 +1,28 @@
+<div class="card card-outline card-primary rounded-0">
+    <div class="card-header">
+        <h4 class="mb-o">List of Contact</h4>
+    </div>
+    <div class="card-body">
+        <div class="container-fluid">
+            <table class="table table-stripped table-bordered">
+                <colgroup>
+                    <col width="10%">
+                    <col width="40%">
+                    <col width="40%">
+                    <col width="10%">
+                </colgroup>
+                <thead>
+                    <tr class="bg-gradient bg-primary text-light">
+                        <th class="py-1 text-center">#</th>
+                        <th class="py-1 text-center">Name</th>
+                        <th class="py-1 text-center">Gender</th>
+                        <th class="py-1 text-center">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <? php if () ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
